40c07b3bc9e6498fe4f48fa4e771434c0046bfd9
fun90 <okbeok@163.com>
2017-11-02 21:19:58 +0800

    修改json配置问题

--- v2ray_install.sh
+++ v2ray_install.sh
@@ -23,76 +23,76 @@ echo "安装证书..."
 ~/.acme.sh/acme.sh --installcert -d $udomain --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
 
 #mkdir /etc/v2ray
-echo "{
-  "log" : {
-    "access": "/var/log/v2ray/access.log",
-    "error": "/var/log/v2ray/error.log",
-    "loglevel": "warning"
+echo -e "{
+  \"log\" : {
+    \"access\": \"/var/log/v2ray/access.log\",
+    \"error\": \"/var/log/v2ray/error.log\",
+    \"loglevel\": \"warning\"
   },
-  "inbound": {
-    "port": 443,
-    "protocol": "vmess",
-    "settings": {
-      "clients": [
+  \"inbound\": {
+    \"port\": 443,
+    \"protocol\": \"vmess\",
+    \"settings\": {
+      \"clients\": [
         {
-          "id": "bc7e3ae8-71e9-4a84-acb0-70cb4c3ff3b7",
-          "level": 1,
-          "alterId": 64
+          \"id\": \"bc7e3ae8-71e9-4a84-acb0-70cb4c3ff3b7\",
+          \"level\": 1,
+          \"alterId\": 64
         }
       ]
     },
-    "streamSettings":{
-      "network": "ws",
-      "security": "tls",
-      "tlsSettings": {
-        "allowInsecure" : false,
-        "certificates": [
+    \"streamSettings\":{
+      \"network\": \"ws\",
+      \"security\": \"tls\",
+      \"tlsSettings\": {
+        \"allowInsecure\" : false,
+        \"certificates\": [
           {
-            "certificateFile": "/etc/v2ray/v2ray.crt",
-            "keyFile": "/etc/v2ray/v2ray.key"
+            \"certificateFile\": \"/etc/v2ray/v2ray.crt\",
+            \"keyFile\": \"/etc/v2ray/v2ray.key\"
           }
         ]
       }
     }
   },
-  "inboundDetour":[
+  \"inboundDetour\":[
     
   ],
-  "outbound": {
-    "protocol": "freedom",
-    "settings": {}
+  \"outbound\": {
+    \"protocol\": \"freedom\",
+    \"settings\": {}
   },
-  "outboundDetour": [
+  \"outboundDetour\": [
     {
-      "protocol": "blackhole",
-      "settings": {},
-      "tag": "blocked"
+      \"protocol\": \"blackhole\",
+      \"settings\": {},
+      \"tag\": \"blocked\"
     }
   ],
-  "routing": {
-    "strategy": "rules",
-    "settings": {
-      "rules": [
+  \"routing\": {
+    \"strategy\": \"rules\",
+    \"settings\": {
+      \"rules\": [
         {
-          "type": "field",
-          "ip": [
-            "0.0.0.0/8",
-            "10.0.0.0/8",
-            "100.64.0.0/10",
-            "127.0.0.0/8",
-            "169.254.0.0/16",
-            "172.16.0.0/12",
-            "192.0.0.0/24",
-            "192.0.2.0/24",
-            "192.168.0.0/16",
-            "198.18.0.0/15",
-            "198.51.100.0/24",
-            "203.0.113.0/24",
-            "::1/128",
-            "fc00::/7",
-            "fe80::/10"
+          \"type\": \"field\",
+          \"ip\": [
+            \"0.0.0.0/8\",
+            \"10.0.0.0/8\",
+            \"100.64.0.0/10\",
+            \"127.0.0.0/8\",
+            \"169.254.0.0/16\",
+            \"172.16.0.0/12\",
+            \"192.0.0.0/24\",
+            \"192.0.2.0/24\",
+            \"192.168.0.0/16\",
+            \"198.18.0.0/15\",
+            \"198.51.100.0/24\",
+            \"203.0.113.0/24\",
+            \"::1/128\",
+            \"fc00::/7\",
+            \"fe80::/10\"
           ],
-          "outboundTag": "blocked"
+          \"outboundTag\": \"blocked\"
         }
       ]
     }
