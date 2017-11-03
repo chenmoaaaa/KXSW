432387258159e1a7341c5defd3a5cf28b8838e3e
fun90 <okbeok@163.com>
2017-11-03 21:01:27 +0800

    websock+tls与tcp+tls方式

--- v2ray_install.sh
+++ v2ray_install.sh
@@ -5,17 +5,14 @@ export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 [ $(id -u) != "0" ] && { echo "Error: You must be root to run this script"; exit 1; }
 
 #V2Ray Install
-echo "准备安装V2Ray，传输方式为无TLS的tcp和有TLS的ws"
+echo "准备安装V2Ray，websock+tls与tcp+tls方式"
 bash <(curl -L -s https://install.direct/go.sh)
 
-read -p "输入TLS域名： " udomain
-read -p "输入TLS端口： " uport
-#无tls的端口
-uport2=$[uport + 1]
-echo "无TLS的TCP端口为：$uport2"
-read -p "输入用户UUID： " uuid
-#read -p "输入传输方式(tcp或ws，默认ws)： " utype
-[ -z "${utype}" ] && utype="ws"
+read -p "输入TLS域名： " domain
+read -p "输入tcp的TLS端口： " port
+read -p "输入ws的TLS端口：" port2
+#read -p "输入传输方式(tcp或ws，默认ws)： " network
+# [ -z "${network}" ] && network="ws"
 #安装 acmey.sh依赖
 apt-get install socat
 #安装 acme.sh
@@ -24,11 +21,12 @@ curl  https://get.acme.sh | sh
 source ~/.bashrc
 #证书生成
 echo "生成证书..."
-~/.acme.sh/acme.sh --issue -d $udomain --standalone -k ec-256
+~/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
 echo "安装证书..."
 #证书安装
-~/.acme.sh/acme.sh --installcert -d $udomain --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
+~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
 #mkdir /etc/v2ray
+UUID=$(cat /proc/sys/kernel/random/uuid)
 echo -e "{
   \"log\" : {
     \"access\": \"/var/log/v2ray/access.log\",
@@ -36,19 +34,19 @@ echo -e "{
     \"loglevel\": \"warning\"
   },
   \"inbound\": {
-    \"port\": $uport,
+    \"port\": $port,
     \"protocol\": \"vmess\",
     \"settings\": {
       \"clients\": [
         {
-          \"id\": \"$uuid\",
+          \"id\": \"$UUID\",
           \"level\": 1,
           \"alterId\": 64
         }
       ]
     },
     \"streamSettings\":{
-      \"network\": \"ws\",
+      \"network\": \"tcp\",
       \"security\": \"tls\",
       \"tlsSettings\": {
         \"allowInsecure\" : true,
@@ -61,21 +59,31 @@ echo -e "{
       }
     }
   },
-  \"inboundDetour\":[
+  \"inboundDetour\": [
     {
-      \"port\": $uport2,
+      \"port\": $port2,
       \"protocol\": \"vmess\",
       \"settings\": {
         \"clients\": [
           {
-            \"id\": \"$uuid\",
+            \"id\": \"$UUID\",
             \"level\": 1,
             \"alterId\": 64
           }
         ]
       },
       \"streamSettings\":{
-        \"network\": \"tcp\"
+        \"network\": \"ws\",
+        \"security\": \"tls\",
+        \"tlsSettings\": {
+          \"allowInsecure\" : true,
+          \"certificates\": [
+            {
+              \"certificateFile\": \"/etc/v2ray/v2ray.crt\",
+              \"keyFile\": \"/etc/v2ray/v2ray.key\"
+            }
+          ]
+        }
       }
     }
   ],
@@ -118,7 +126,12 @@ echo -e "{
       ]
     }
   }
-}
-" > /etc/v2ray/config.json
+}" > /etc/v2ray/config.json
+
+echo "安装完成，传输方式为ws+tls和tcp+tls。"
+echo "域名：$domain"
+echo "tcp端口：$port, ws端口：$port2"
+echo "uuid：$UUID"
+
 
 service v2ray restart
